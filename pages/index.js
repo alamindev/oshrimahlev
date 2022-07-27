a4a85251cd65b54d1fb5a3581089a626c8c8aaf9
alamindev <mdalamin6554@gmail.com>
2022-07-27 12:32:55 +0600

    added

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import Navbar from "../components/navbar";
 import Banner from "../components/banner";
 import BrandLogo from "../components/brand";
-import Courses from "../components/Courses";
+import Courses from "../components/courses";
  
 export default function Home() {
   return (
